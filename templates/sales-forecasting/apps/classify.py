e9f53f3b057cfb1e4f173b3470a1830835fd5a26
Martin Roed <msroed@gmail.com>
2022-03-18 12:23:28 +0100

    fix: rewrite to new structure + reqs.txt

--- templates/sales-forecasting/apps/classify.py
+++ templates/sales-forecasting/apps/classify.py
@@ -1,32 +1,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from st_aggrid import AgGrid, GridOptionsBuilder
-
-st.title('Web-store sales')
-
-stats = ['Quantity', 'Sales', 'Profit', 'Discount']
-frames = {}
-for stat in stats:
-    frames[stat] = pd.read_csv('../data/generated/'+stat+'.csv')
+import databutton as db
 
 
-SubCategories = frames['Sales']['Sub-Category'].unique()
-Cities = frames['Sales']['City'].unique()
+@db.streamlit('/app/classify')
+def app():
+    st.title('Web-store sales')
 
+    stats = ['Quantity', 'Sales', 'Profit', 'Discount']
+    frames = {}
+    for stat in stats:
+        frames[stat] = pd.read_csv('./data/generated/'+stat+'.csv')
 
-option = st.selectbox('Quantity', options=stats)
-disp = frames[option].groupby('Sub-Category').sum()
+    SubCategories = frames['Sales']['Sub-Category'].unique()
+    Cities = frames['Sales']['City'].unique()
 
-# if(st.button(label='All')):
-#     disp = frames['Quantity']
-# if(st.button(label='Category')):
-#     disp = frames['Quantity'].groupby('Category').sum()
-# if(st.button(label='Sub-Category')):
-#     disp = frames['Quantity'].groupby('Sub-Category').sum()
-# if(st.button(label='City')):
-#     disp = frames['Quantity'].groupby('City').sum()
+    option = st.selectbox('Quantity', options=stats)
+    disp = frames[option].groupby('Sub-Category').sum()
 
+    # if(st.button(label='All')):
+    #     disp = frames['Quantity']
+    # if(st.button(label='Category')):
+    #     disp = frames['Quantity'].groupby('Category').sum()
+    # if(st.button(label='Sub-Category')):
+    #     disp = frames['Quantity'].groupby('Sub-Category').sum()
+    # if(st.button(label='City')):
+    #     disp = frames['Quantity'].groupby('City').sum()
 
-
-st.dataframe(disp)
+    st.dataframe(disp)
