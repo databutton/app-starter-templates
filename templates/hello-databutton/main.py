945f36c3267a907b2e23dc4ff8f7d49543ce93ec
Martin Roed <msroed@gmail.com>
2022-05-25 23:16:21 +0200

    fix: Update hello-databutton templatE

--- templates/hello-databutton/main.py
+++ templates/hello-databutton/main.py
@@ -1,6 +1,26 @@
 import databutton as db
 import streamlit as st
 
-@db.streamlit(route='/hello', name="Hello Databutton", cpu=1, memory='2056Gi')
+
+@db.streamlit(route="/hello", name="Hello Databutton")
 def hello():
-    st.title('Hello, Databutton')
+    st.title("Hello, Databutton")
+
+
+@db.streamlit(route="/tutorial", name="Tutorial")
+def tutorial():
+    st.title("Tutorial")
+
+
+@db.streamlit(route="/compute", name="Compute intensive app", cpu=8, memory="16Gi")
+def compute():
+    st.title("Compute")
+
+
+@db.repeat(seconds=10 * 60)
+def repeating_job():
+    # Check for new data
+    # Do some work on that data
+    # Write that data to db.dataframes
+    # Send slack notification
+    print("Success!")
